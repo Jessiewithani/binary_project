467d4361ce3f87d6d5a8d35e51abfb601bc7d263
Jessie Le-Ho <lehoj@oregonstate.edu>
2019-11-05 01:57:56 -0700

    Fix test that says that should be able to return characters

--- src/Container/CharacterCardContainer/CharacterCardContainer.test.js
+++ src/Container/CharacterCardContainer/CharacterCardContainer.test.js
@@ -15,49 +15,48 @@ describe('CharacterCardContainer', () => {
 describe('mapStateToProps in CharacterCardContainer', () => {
   it.skip('should return an array with the characters information in it', () => {
     const mockState = {
-      characters: [{
-        id: 17,
-        name: 'Annie',
-        status: 'Alive',
-        species: 'Human',
-        type: '',
-        gender: 'Female',
-        location: 'Anatomy Park',
-        image: 'https://rickandmortyapi.com/api/character/avatar/17.jpeg'
-      }, {
-        id: 18,
-        name: 'Antenna Morty',
-        status: 'Alive',
-        species: 'Human',
-        type: 'Human with antennae',
-        gender: 'Male',
-        location: 'Citadel of Ricks',
-        image: 'https://rickandmortyapi.com/api/character/avatar/18.jpeg'
-      
-    }],
+      characters: {  
+        info: { 
+            count: 3, 
+            pages: 34, 
+            next: 'https://next.com',
+            prev: ''},
+        results: [ 
+            {
+                id: 1, 
+                name: 'Rick Sanchez', 
+                status: 'Alive', 
+                species: 'Human', 
+                type: '', 
+                gender: 'Male', 
+                    location: 'Earth (Replacement Dimension)', 
+                image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+            }
+        ]
+    },
       saveCharacters: 'SAVE_CHARACTERS'
     }
 
-    const expected =  [{
-      id: 17,
-      name: 'Annie',
-      status: 'Alive',
-      species: 'Human',
-      type: '',
-      gender: 'Female',
-      location: 'Anatomy Park',
-      image: 'https://rickandmortyapi.com/api/character/avatar/17.jpeg'
-    }, {
-      id: 18,
-      name: 'Antenna Morty',
-      status: 'Alive',
-      species: 'Human',
-      type: 'Human with antennae',
-      gender: 'Male',
-      location: 'Citadel of Ricks',
-      image: 'https://rickandmortyapi.com/api/character/avatar/18.jpeg'
-    
-  }]
+    const expected = {  
+      info: { 
+          count: 3, 
+          pages: 34, 
+          next: 'https://next.com',
+          prev: ''},
+      results: [ 
+          {
+              id: 1, 
+              name: 'Rick Sanchez', 
+              status: 'Alive', 
+              species: 'Human', 
+              type: '', 
+              gender: 'Male', 
+                  location: 'Earth (Replacement Dimension)', 
+              image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+          }
+      ]
+  }
+  
   
     const mappedProps = mapStateToProps(mockState);
 
--- src/reducers/tests/charactersReducer.test.js
+++ src/reducers/tests/charactersReducer.test.js
@@ -8,34 +8,56 @@ describe('charactersReducer', () => {
 
         expect(result).toEqual(expected)
     });
-    it.skip('should be able to return characters', () => {
+    it('should be able to return characters', () => {
         const initialState = [];
         const action = {
             type: 'SAVE_CHARACTERS',
-            characters: [
-                {   id: 1, 
+            characters: {  
+                info: { 
+                    count: 3, 
+                    pages: 34, 
+                    next: 'https://next.com',
+                    prev: ''},
+                results: [ 
+                    {
+                        id: 1, 
+                        name: 'Rick Sanchez', 
+                        status: 'Alive', 
+                        species: 'Human', 
+                        type: '', 
+                        gender: 'Male', 
+                            location: 'Earth (Replacement Dimension)', 
+                        image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+                    }
+                ]
+            }
+            
+        }
+
+        const expected = [{  
+            info: { 
+                count: 3, 
+                pages: 34, 
+                next: 'https://next.com',
+                prev: ''},
+            results: [ 
+                {
+                    id: 1, 
                     name: 'Rick Sanchez', 
                     status: 'Alive', 
                     species: 'Human', 
                     type: '', 
                     gender: 'Male', 
-                    location: 'Earth (Replacement Dimension)', 
-                    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'}
+                        location: 'Earth (Replacement Dimension)', 
+                    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+                }
             ]
-        }
-        const expected = [
-            {   id: 1, 
-                name: 'Rick Sanchez', 
-                status: 'Alive', 
-                species: 'Human', 
-                type: '', 
-                gender: 'Male', 
-                location: 'Earth (Replacement Dimension)', 
-                image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'}
-        ]
+        }]
 
-        const result = charactersReducer(initialState, action);
-
+        const result = charactersReducer(initialState, action)
+        
         expect(result).toEqual(expected)
+        
     })
+    
 })
