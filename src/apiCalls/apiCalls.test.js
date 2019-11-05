34399ad51e5e5ce74b4e4bd5cabf160e3b876087
Jessie Le-Ho <lehoj@oregonstate.edu>
2019-11-05 01:09:24 -0700

    Make tests for apiCalls

--- src/apiCalls/apiCalls.test.js
+++ src/apiCalls/apiCalls.test.js
@@ -1,8 +1,49 @@
-//download all methods
+import { getCharacters } from '../apiCalls/apiCalls'
 
 describe('apiCalls', () => {
-    it.skip('should...', () => {
-  
+
+    describe('getCharacters', () => {
+      let mockCharacters = [
+        {   id: 1, 
+          name: 'Rick Sanchez', 
+          status: 'Alive', 
+          species: 'Human', 
+          type: '', 
+          gender: 'Male', 
+          location: 'Earth (Replacement Dimension)', 
+          image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'}
+      ]
+      beforeEach(() => {
+        window.fetch = jest.fn().mockImplementation(() => {
+          return Promise.resolve({
+            ok: true,
+            json: () => Promise.resolve(mockCharacters)
+          })
+        })
+      })
+      it('should call fetch with the correct url', () => {
+        getCharacters();
+
+        expect(window.fetch).toHaveBeenCalledWith('https://rickandmortyapi.com/api/character')
+      })
+      it('should return an array of characters', () => {
+        getCharacters()
+          .then(results => expect(results).toEqual(mockCharacters))
+      })
+      it('should return an error', () => {
+        window.fetch = jest.fn().mockImplementation(() => {
+          return Promise.resolve({
+            ok: false
+          })
+        })
+        expect(getCharacters()).rejects.toEqual(Error('could not get characters'))
+      })
+      it('should return an error if promise rejects', () => {
+        window.fetch = jest.fn().mockImplementation(() => {
+          return Promise.reject(Error('fetch failed'))
+        })
+        expect(getCharacters()).rejects.toEqual(Error('fetch failed'))
+      })
     })
   })
   
