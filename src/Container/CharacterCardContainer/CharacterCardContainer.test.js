14634efbf832bc1513801fb880469b1619cdb9ee
Jessie Le-Ho <lehoj@oregonstate.edu>
2019-11-05 21:44:46 -0700

    Change mock data for snapshot and also added in more props

--- src/Container/CharacterCardContainer/CharacterCardContainer.test.js
+++ src/Container/CharacterCardContainer/CharacterCardContainer.test.js
@@ -5,7 +5,39 @@ import { shallow } from 'enzyme'
 
 describe('CharacterCardContainer', () => {
   it.skip('should match the snapshot with all the correct data passed in', () => {
-    const wrapper = shallow(<CharacterCardContainer species={jest.fn()} filteredCharacters={jest.fn()}/>)
+    const mockCharacters = {info: {
+      count: 493,
+      pages: 25,
+      next: 'http://rickandmortyapi.com/api/character/?page=2',
+      prev: ''
+    },
+    results: [
+      {
+        id: 1,
+        name: 'Rick Sanchez',
+        status: 'Alive',
+        species: 'Human',
+        type: '',
+        gender: 'Male',
+        origin: {
+          name: 'Earch (C-137)',
+          url: 'https://rickandmortyapi.com/api/location/1'
+        },
+        location: {
+          name: 'Earth (Replacement Dimension)',
+          url: 'https://rickandmortyapi.com/api/location/20'
+        },
+        image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+        episode: [
+          "https://rickandmortyapi.com/api/episode/1",
+          "https://rickandmortyapi.com/api/episode/2",
+        ],
+        url: 'https://rickandmortyapi.com/api/character/1',
+        created: '2017-11-04T18:48:46.250Z'
+      }
+    ]
+    }
+    const wrapper = shallow(<CharacterCardContainer species={jest.fn()} characters={mockCharacters.results}/>)
     expect(wrapper).toMatchSnapshot()
   })
     
